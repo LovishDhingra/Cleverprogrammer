e541e66e627540d9e7a60d503f2c78e9821e7bc7
Lovish Dhingra <lovishdhingra2023@gmail.com>
2026-06-22 23:57:37 +0530

    first commit

---
+++ SmartX/server/controllers/authContoller.js
@@ -0,0 +1,57 @@
+const OTP = require("../models/otp");
+const User = require("../models/user");
+const otpGenerator = require("otp-generator");
+
+exports.createOpt = async(req,res)=>{
+  try {
+
+    const {email} = req.body;
+
+    if(!email){
+        return res.status(400).json({
+            success:false,
+            message:"please fill all the input fileds",
+        });
+    }
+
+    const userDetails = await User.findOne({email:email});
+
+    if(userDetails){
+        return res.status(400).json({
+            success:false,
+            message:"User allready have an account",
+        })
+    }
+     
+
+  const newOtp = otpGenerator.generate(4,{
+    specialChars:false,
+    upperCaseAlphabets:false,
+    lowerCaseAlphabets:false,
+  });
+
+
+    const newOpt = await OTP.create({
+        email:email,
+        otp:newOtp,
+    });
+
+
+
+    return res.status(200).json({
+        success:true,
+        message:"Otp created successfully",
+        newOpt,      
+    });
+
+  
+    } catch (error) {
+       console.log(error);
+       return res.status(500).json({
+        success:false,
+        message:"Internal Setver error",
+       })
+        
+    }
+
+}
